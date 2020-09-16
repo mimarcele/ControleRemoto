6c3de69ce15e70a0331a5ab5efa02e6030eadf3c
Milena Marcele <milenamarcele777@gmail.com>
2020-09-15 21:31:27 -0300

    Extensão da classe controle remoto, a classe contém algumas funções que o controle remoto pode exercer.

---
+++ src/Encapsulamento/ControleRemotoImpl.java
@@ -0,0 +1,53 @@
+package Encapsulamento;
+
+public class ControleRemotoImpl extends ControleRemoto {
+        @Override
+        public void ligar() {
+
+        }
+
+        @Override
+        public void desligar() {
+
+        }
+
+        @Override
+        public void abrirMenu() {
+
+        }
+
+        @Override
+        public void fecharMenu() {
+
+        }
+
+        @Override
+        public void maisVolume() {
+
+        }
+
+        @Override
+        public void menosVolume() {
+
+        }
+
+        @Override
+        public void ligarMudo() {
+
+        }
+
+        @Override
+        public void desligarMudo() {
+
+        }
+
+        @Override
+        public void play() {
+
+        }
+
+        @Override
+        public void pause() {
+
+        }
+    }
